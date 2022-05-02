4ebb3165465016876f131a8e0c1c082f24aca1b1
fabriziofbarros <fabriziofbarros@gmail.com>
2022-05-02 12:05:06 -0300

    javasvript

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log("MSG GERADA NO ARQ EXTERNO OLÁ MUNDO")
+
+var nome = fa
+console.log("Ola " + fa)
+
+function exemplo() {
+    let sobrenome = "franco"
+    return sobrenome
+}
+
+console.log('Nome completo ' + nome + exemplo())
+
+const logoTit = document.querySelector("#tit-logo")
+logoTit.innerHTML = "Teste"
+console.log("Conteudo do elemento : " + logoTit.innerHTML)
